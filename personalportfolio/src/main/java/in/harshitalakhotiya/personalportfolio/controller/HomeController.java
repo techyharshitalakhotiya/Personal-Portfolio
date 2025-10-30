c346eab78e4796b2b6cf5a47c1bd73dc26e2522e
techyharshitalakhotiya <harshitalakhotiya@gmail.com>
2025-10-30 21:30:36 +0530

    first commit

---
+++ personalportfolio/src/main/java/in/harshitalakhotiya/personalportfolio/controller/HomeController.java
@@ -0,0 +1,13 @@
+package in.harshitalakhotiya.personalportfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({"/", "", "/home"})
+    public String showHomePage() {
+        return "home";
+    }
+}
